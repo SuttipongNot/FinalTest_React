88d74454cec08b4f40cb7b4eb9b6310d60ce372e
Methaporn Limrostham <pedza507@gmail.com>
2024-10-22 11:01:48 +0700

    SigninAdmin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import SignInUser from './components/SignInAdmin';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signinadmin" />} /> 
+        <Route exact path='/signinadmin' element={<SignInUser />} />
+        <Route exact path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
